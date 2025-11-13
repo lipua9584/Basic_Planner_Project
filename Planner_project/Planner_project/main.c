ad6e4012e9add7a6d130b317f28e9eba05e3b745
lipua9584 <wintour768@gmail.com>
2025-11-13 16:53:13 +0900

    코드 수정 1.1

--- Planner_project/Planner_project/main.c
+++ Planner_project/Planner_project/main.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 
-//typedef struct {
-//	char plan_name[20];
-//	int  plan_year;
-//	int  plan_month;
-//	int  plan_day;
-//	int  plan_time;
-//	char plan_task[100];
-//};
-
 int mainmenu(void);
+void newplan(void);
+void checkplan(void);
 
-int main(int argv, char* argc[])
+int main(void)
 {
+	char menu;
+
 	while (1) {
-		char menu;
-		mainmenu();
+		
+		menu = mainmenu();
+		switch (menu) {
+		case 0:
+			printf("프로그램을 종료합니다.\n");
+			return 0;
+			break;
+		case 1:
+			newplan();
+			break;
+		case 2:
+			//checkplan();
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			printf("메뉴를 잘못 선택하셨습니다. 다시 선택해주세요.\n");
+			break;
+		}
 	}
 	return 0;
 }
@@ -23,7 +39,7 @@ int main(int argv, char* argc[])
 int mainmenu(void)
 {
 	int menu = 0;
-	printf("=====================플래너 프로그램====================\n");
+	printf("==================플래너 프로그램 1.1v==================\n");
 	printf("|                                                      |\n");
 	printf("------------------------메뉴----------------------------\n");
 	printf("|                                                      |\n");
@@ -38,3 +54,36 @@ int mainmenu(void)
 	scanf_s("%d", &menu);
 	return menu;
 }
+
+void newplan(void)
+{
+	char pname[100];
+	int pyear, pmonth, pday, ptime;
+	char ptask[100];
+
+	printf("계획 이름: ");
+	scanf_s("%s", pname, (int)sizeof(pname));
+	printf("날짜(xx(년도) x(월) x(일)): ");
+	scanf_s("%d %d %d", &pyear, &pmonth, &pday);
+	printf("시간(xx(24h)): ");
+	scanf_s("%d", &ptime);
+	printf("계획 내용: ");
+	scanf_s("%s", ptask, (int)sizeof(ptask));
+
+	printf("\n저장 완료!\n");
+
+	printf("계획 이름: %s\n", pname);
+	printf("날짜: %d년 %d월 %d일\n", pyear, pmonth, pday);
+	printf("시간: %d시\n", ptime);
+	printf("계획 내용: %s\n\n", ptask);
+}
+
+//void checkplan(void)
+//{
+//	printf("기존 계획을 확인합니다.\n\n");
+//	for (/*계획의 갯수만큼*/) {
+//		printf("%d번. %s");
+//		
+//	}
+//	
+//}
